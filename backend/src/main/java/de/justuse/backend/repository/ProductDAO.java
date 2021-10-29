7b6151c82afd14e316d2ae28a060a46c1461f1d3
Philipp Steiner <philipp-steiner@gmx.de>
2021-10-29 12:11:24 +0200

    add ProductSerivce and add method

--- backend/src/main/java/de/justuse/backend/repository/ProductDAO.java
+++ backend/src/main/java/de/justuse/backend/repository/ProductDAO.java
@@ -1,13 +1,13 @@
 package de.justuse.backend.repository;
 
+import de.justuse.backend.model.Product;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
-public interface ProductDAO extends PagingAndSortingRepository<ProductDAO, String> {
+public interface ProductDAO extends PagingAndSortingRepository<Product, String> {
 
     @Override
-    List<ProductDAO> findAll();
+    List<Product> findAll();
 }
---
+++ backend/src/main/java/de/justuse/backend/service/ProductService.java
@@ -0,0 +1,23 @@
+package de.justuse.backend.service;
+
+import de.justuse.backend.model.Product;
+import de.justuse.backend.repository.ProductDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductDAO productRepo;
+
+    @Autowired
+    public ProductService(ProductDAO productRepo) {
+        this.productRepo = productRepo;
+    }
+
+
+    public Product addProduct(Product product){
+      return productRepo.save(product);
+    }
+
+}
