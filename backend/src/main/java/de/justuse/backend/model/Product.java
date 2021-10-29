bd1f87a38884b875b99b92ce7873988a571285df
Philipp Steiner <philipp-steiner@gmx.de>
2021-10-29 11:37:46 +0200

    add product model and enum for location

---
+++ backend/src/main/java/de/justuse/backend/model/Product.java
@@ -0,0 +1,21 @@
+package de.justuse.backend.model;
+
+import de.justuse.backend.enums.Location;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+
+    private final String id;
+    private String title;
+    private String description;
+    private int amount;
+    private boolean isAvailable;
+    private final int MAX_RENTAL_CYCLE;
+    private Location location;
+    private double price;
+    private boolean isFavorite;
+
+}
