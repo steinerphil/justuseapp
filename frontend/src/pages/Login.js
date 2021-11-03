7b974e8ea3b6e961d9f34819fde14e50e12b39b9
Philipp Steiner <philipp-steiner@gmx.de>
2021-11-03 22:25:34 +0100

    add custom css

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -21,7 +21,6 @@ export default function Login() {
     });
 
 
-
     const handleChange = (prop) => (event) => {
         setValues({...values, [prop]: event.target.value});
     };
@@ -43,46 +42,91 @@ export default function Login() {
     }
 
     return (
-        <>
-                <FormControl sx={{m: 1, width: '25ch'}} variant="outlined">
-                    <InputLabel htmlFor="outlined-adornment-username">Benutzername</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-username"
-                        value={values.username}
-                        onChange={handleChange('username')}
-                        label="Benutzername"
-                    />
-                </FormControl>
+        <LoginContainer>
+            <StyledP>Login</StyledP>
+            <StyledFormControl sx={{m: 1}} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-username">Benutzername</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-username"
+                    value={values.username}
+                    onChange={handleChange('username')}
+                    label="Benutzername"
+                />
+            </StyledFormControl>
 
-                <FormControl sx={{m: 1, width: '25ch'}} variant="outlined">
-                    <InputLabel htmlFor="outlined-adornment-password">Passwort</InputLabel>
-                    <OutlinedInput
-                        id="outlined-adornment-password"
-                        type={values.showPassword ? 'text' : 'password'}
-                        value={values.password}
-                        onChange={handleChange('password')}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleClickShowPassword}
-                                    onMouseDown={handleMouseDownPassword}
-                                    edge="end"
-                                >
-                                    {values.showPassword ? <VisibilityOff/> : <Visibility/>}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                        label="Passwort"
-                    />
-                </FormControl>
-                <SubmitButton variant="contained" onClick={handleSubmit}>Anmelden</SubmitButton>
-        </>
+            <StyledFormControl sx={{m: 1}} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Passwort</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={values.showPassword ? 'text' : 'password'}
+                    value={values.password}
+                    onChange={handleChange('password')}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                            >
+                                {values.showPassword ? <VisibilityOff/> : <Visibility/>}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    label="Passwort"
+                />
+            </StyledFormControl>
+            <SubmitButton variant="contained" onClick={handleSubmit}>Anmelden</SubmitButton>
+        </LoginContainer>
     )
 }
 
+const StyledP = styled.p`
+  font-size: 1.6rem;
+  line-height: 1.5rem;
+  font-weight: 600;
+
+  @media (min-width: 573px) {
+    font-size: 1.9rem;
+    line-height: 1.75rem;
+    margin-top: 1.25rem;
+    margin-left: 0;
+    margin-right: 0;
+  }
+`
+
 const SubmitButton = styled(Button)`
-    &&{
-      text-transform: none;
-    }
+  && {
+    text-transform: none;
+    width: 100%
+  }
+`
+
+const LoginContainer = styled.div`
+  font-family: 'Nunito', sans-serif;
+  display: flex;
+  flex-direction: column;
+  max-width: 45%;
+  margin: 10% auto 0 auto;
+  align-items: center;
+  
+  @media(min-width: 1000px){
+    width: 450px;
+  }
+  
+  @media(max-width: 680px){
+    max-width: 70%;
+  }
+
+  @media(max-width: 445px){
+    max-width: 87%;
+  }
+`
+
+const StyledFormControl = styled(FormControl)`
+  && {
+    margin: 0 0 10% 0;
+    width: 100%;
+  }
+
 `
