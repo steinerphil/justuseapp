2d6eba6446a29e8d35d570b2edaab0ee38af8d74
Philipp Steiner <philipp-steiner@gmx.de>
2021-11-04 11:43:21 +0100

    optimize imports

--- backend/src/main/java/de/justuse/backend/service/api/GithubApiService.java
+++ backend/src/main/java/de/justuse/backend/service/api/GithubApiService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 
 @Service
