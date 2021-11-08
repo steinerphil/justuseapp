2b661a6b97180ef4498b721da41154090f6d305b
steinerphil <74187172+steinerphil@users.noreply.github.com>
2021-11-08 09:56:55 +0100

    Update productApiService.js

--- frontend/src/service/productApiService.js
+++ frontend/src/service/productApiService.js
@@ -1,13 +1,5 @@
 import axios from 'axios';
 
-// const getHeader = (token) => {
-//     return {
-//         headers: {
-//             Authorization: `Bearer ${token}`
-//         },
-//     }
-// }
-
 
 //do no need token because route is not protected
 export function getProducts() {
