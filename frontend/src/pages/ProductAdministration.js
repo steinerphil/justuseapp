9311ba7df2bf9dc6060ffa4e02928d6ef902042b
Philipp Steiner <philipp-steiner@gmx.de>
2021-11-16 13:15:39 +0100

    update administration page

--- frontend/src/pages/ProductAdministration.js
+++ frontend/src/pages/ProductAdministration.js
@@ -1,11 +1,56 @@
-import NewProduct from "./NewProduct";
+import {useHistory} from "react-router-dom";
+import styled from "styled-components/macro";
+
 
 export default function ProductAdministration() {
 
-return(
-    <>
-        <NewProduct/>
-    </>
+    const history = useHistory()
+
+    return (
+        <Wrapper>
+            <Headline>Wähle eine Aktion aus:</Headline>
+            <ButtonDiv onClick={() => history.push("/administration/new")}>
+                <p>Produkt anlegen</p>
+            </ButtonDiv>
+            <ButtonDiv onClick={() => history.push("/administration/delete")}>
+                <p>Produkt löschen</p>
+            </ButtonDiv>
+            <ButtonDiv>
+                <p>Produkt bearbeiten</p>
+            </ButtonDiv>
+        </Wrapper>
+
 
     )
 }
+
+const Wrapper = styled.div`
+  font-family: 'Nunito', sans-serif;
+  display: flex;
+  margin-top: 90px;
+  text-align: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Headline = styled.p`
+  font-size: 1.2rem;
+  line-height: 1.5rem;
+  font-weight: 400;
+`
+
+const ButtonDiv = styled.div`
+  margin: 1.5%;
+  width: 120px;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  background-color: #DDDDDD;
+  padding: 1.4%;
+  border-radius: 30px;
+  
+  :hover{
+    background-color: #30475E;
+    color: white;
+  }
+`
